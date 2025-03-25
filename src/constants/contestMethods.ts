b98c181bd3cf408bb8049ed66c74aa6666787120
Nguyen Ngoc Anh Tuan <Tomtepfa@gmail.com>
2025-03-25 23:50:26 +0700

    feat(contestMethods): update contest methods with new entries

--- src/constants/contestMethods.ts
+++ src/constants/contestMethods.ts
@@ -5,8 +5,15 @@ export const ContestMethods = {
     17: "Vấn đáp",
     18: "TNKQ",
     19: "Thực hành máy",
-    21: "TNKQ(60P)",
-    22: "TNKQ(70P)",
+    21: "TNKQ (60P)",
+    22: "TNKQ (70P)",
     23: "Đồ án",
     24: "Thực hành",
+    25: "Tiểu luận",
+    27: "Vấn đáp online",
+    28: "TNKQ (50P)",
+    29: "Tiểu luận (Có báo cáo)",
+    30: "TNKQ (50P_QP)",
+    1030: "Thực hành ngoài trời",
+    1031: "TNKQ (40P)"
 };
